7646fabb1358fad607090e5023752f6fff561828
Your Name <you@example.com>
2025-03-03 05:03:57 +0200

    slider

--- JS/main.js
+++ JS/main.js
@@ -16,4 +16,25 @@ document.addEventListener("DOMContentLoaded", function () {
       576: { slidesPerView: 1 },
     },
   });
+  var swiper2 = new Swiper(".myCustomSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".custom-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".custom-next",
+      prevEl: ".custom-prev",
+    },
+  });
 });
+
+/* -------------range function-------------------------------*/
+const slider = document.getElementById("electricBill");
+const output = document.getElementById("sliderValue");
+
+slider.oninput = function () {
+  output.innerText = `$${this.value}`;
+};
