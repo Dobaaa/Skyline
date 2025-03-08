aa6dee3cb025fcb15b9d3f8a803a76a7ef98cb05
Your Name <you@example.com>
2025-03-08 21:28:23 +0200

    last update

--- JS/main.js
+++ JS/main.js
@@ -38,3 +38,14 @@ const output = document.getElementById("sliderValue");
 slider.oninput = function () {
   output.innerText = `$${this.value}`;
 };
+
+function updateSliderBg() {
+  let val = slider.value;
+  let min = slider.min;
+  let max = slider.max;
+  let percentage = ((val - min) / (max - min)) * 100;
+  slider.style.background = `linear-gradient(to right, #f9b061 ${percentage}%, #c55402 ${percentage}%)`;
+}
+
+slider.addEventListener("input", updateSliderBg);
+updateSliderBg(); // لضبط الخلفية عند التحميل
